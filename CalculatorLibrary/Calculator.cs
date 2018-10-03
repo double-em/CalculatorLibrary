b7de59da326fac92fe65464cc32f85b4f9ecf961
Zxited <mmgamesfull@gmail.com>
2018-10-03 09:30:14 +0200

    The Setup

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static int Add(double v1, double v2) //Kaare
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Subtract(double v1, double v2) //Anders
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Multiply(double v1, double v2) //Brian
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Divide(double v1, double v2) //Mathias
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Sum(double[] numbers) //Whoever
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
